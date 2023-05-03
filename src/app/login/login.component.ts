1e21dd24b81c5bde41344a1f6e0d4f3e081a9abb
shihas ks <shihaskz123@gmail.com>
2023-05-03 16:28:28 +0530

    first commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  
+  data="Your Perfect Banking Partner"
+
+  placeHolderData="Enter Acno"
+
+  constructor() {
+    
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  login(){
+    alert("login clicked")
+  }
+
+  unameChange(event:any){
+    console.log(event.target.value);
+    
+
+  }
+}
